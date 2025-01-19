4dce8b0050a073ee68c4d9d6a7d2db7aabe6e070
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-01-19 22:38:20 +0600

    update home page and layout componet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import TestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChoseUs from "@/components/WhyChoseUs";
 
 export default function Home() {
 	return (
 		<main className="min-h-screen bg-black/95 antialiased bg-grid-white/[0.05]">
-      <HeroSection />
+			<HeroSection />
+			<FeaturedCourses />
+			<WhyChoseUs />
+			<TestimonialCards />
+			<UpcomingWebinars />
+			<Instructors />
 		</main>
 	);
 }
